b581bb58ade50fef7f79b0be5e4d2b5bef5bae97
John Edmondson <edmondsonj@gmail.com>
2023-02-21 12:51:23 -0800

    testing the truncated version of the Text component

--- DoneWithIt/App.tsx
+++ DoneWithIt/App.tsx
@@ -1,26 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
   // console.log("Hello World");
   let x = 1;
   // x.toString();
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Hello World</Text>
       <StatusBar style="auto" />
       <View>
-        <Text>Hi</Text>
-
-        <View>
-          <Text>Hi</Text>
-
-          <View>
-            <Text>Hi</Text>
-          </View>
-        </View>
+        <Text>Hi, my name is John.</Text>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -28,7 +20,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
