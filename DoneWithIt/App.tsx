d31900cbae204abfd8e56a1d1fd9bc183c9aba12
John Edmondson <edmondsonj@gmail.com>
2023-02-21 12:45:58 -0800

    experiment with View components

--- DoneWithIt/App.tsx
+++ DoneWithIt/App.tsx
@@ -3,12 +3,23 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   // console.log("Hello World");
-  // let x;
+  let x = 1;
   // x.toString();
   return (
     <View style={styles.container}>
       <Text>Hello World</Text>
       <StatusBar style="auto" />
+      <View>
+        <Text>Hi</Text>
+
+        <View>
+          <Text>Hi</Text>
+
+          <View>
+            <Text>Hi</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
