0d6dc0203613870a5cf3c469d319445150424ba9
John Edmondson <edmondsonj@gmail.com>
2023-02-21 12:55:21 -0800

    testing onPress event handler

--- DoneWithIt/App.tsx
+++ DoneWithIt/App.tsx
@@ -10,7 +10,11 @@ export default function App() {
       <Text>Hello World</Text>
       <StatusBar style="auto" />
       <View>
-        <Text>Hi, my name is John.</Text>
+        <Text numberOfLines={1} onPress={() => console.log("test")}>
+          Hi, my name is John. I am trying to type this in a really long line to
+          see and example of truncated text for testing the react Native Text
+          Component limitations on the IOS platform.
+        </Text>
       </View>
     </SafeAreaView>
   );
